3dc1eb2e16810c80125d63b6a44fd0a8ff00686b
abrar <abrarmashuk@gmail.com>
2026-06-01 13:22:48 +0100

    feat: add company struct with Companies House fields

---
+++ internal/company/company.go
@@ -0,0 +1,10 @@
+package company
+
+type Company struct {
+	Name             string `json:"company_name"`
+	Number           string `json:"company_number"`
+	Status           string `json:"company_status"`
+	DateOfCreation   string `json:"date_of_creation"`
+	AccountsOverdue  bool   `json:"accounts_overdue"`
+	LastAccountsDate string `json:"last_accounts_date"`
+}
